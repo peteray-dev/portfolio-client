0c072737afefcb524d6a9582ced7ea48d2a3111c
Ayomide Popoola <popoolaayomide37@gmail.com>
2021-05-17 14:00:23 -0700

    updated works

--- index.js
+++ index.js
@@ -87,16 +87,16 @@ app.all('*' , (err, req, res, next)=>{
 
 //--------------------------------------------//
 
-const configs = {
-  development: {
-    SERVER_URI: 'localhost:5000',
-  },
-  production: {
-    SERVER_URI: 'HEROKU_URI',
-  },
-};
+// const configs = {
+//   development: {
+//     SERVER_URI: 'localhost:5000',
+//   },
+//   production: {
+//     SERVER_URI: 'HEROKU_URI',
+//   },
+// };
 
-module.exports.config = configs[process.env.NODE_ENV]
+// module.exports.config = configs[process.env.NODE_ENV]
 
 //------------------------------------------//
 
